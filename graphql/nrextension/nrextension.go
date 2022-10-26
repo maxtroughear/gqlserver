86abeac9c1a12b20e8688c58acab091cf964000d
Max Troughear <max@maxtroughear.dev>
2022-10-26 21:35:45 +1300

    Remove debug logging. Handle operation segment using context and response interceptor

--- graphql/nrextension/nrextension.go
+++ graphql/nrextension/nrextension.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/maxtroughear/gqlserver/middleware"
 	"github.com/newrelic/go-agent/v3/newrelic"
 )
+
+type operationSegmentContextKey struct{}
 
 type NrExtension struct {
 }
@@ -16,6 +17,7 @@ var _ interface {
 	graphql.HandlerExtension
 	graphql.OperationInterceptor
 	graphql.FieldInterceptor
+	graphql.ResponseInterceptor
 } = NrExtension{}
 
 func (n NrExtension) ExtensionName() string {
@@ -29,24 +31,21 @@ func (n NrExtension) Validate(schema graphql.ExecutableSchema) error {
 func (n NrExtension) InterceptOperation(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
 	tx := newrelic.FromContext(ctx)
 	oc := graphql.GetOperationContext(ctx)
-	logger := middleware.LogrusFromContext(ctx)
 
 	opName := buildOperationName(oc.OperationName)
 
 	if tx != nil {
 		tx.SetName(opName)
-		defer tx.StartSegment(opName).End()
+		segment := tx.StartSegment(opName)
+		ctx = context.WithValue(ctx, operationSegmentContextKey{}, segment)
 	}
 
-	nextResult := next(ctx)
-	logger.Warn("Operation after next")
-	return nextResult
+	return next(ctx)
 }
 
 func (n NrExtension) InterceptField(ctx context.Context, next graphql.Resolver) (interface{}, error) {
 	tx := newrelic.FromContext(ctx)
 	fc := graphql.GetFieldContext(ctx)
-	logger := middleware.LogrusFromContext(ctx)
 
 	if fc.IsResolver && tx != nil {
 		defer tx.StartSegment(buildResolverName(fc.Field.Name)).End()
@@ -60,7 +59,14 @@ func (n NrExtension) InterceptField(ctx context.Context, next graphql.Resolver) 
 		}
 	}()
 
-	logger.Warn("Field before next")
+	return next(ctx)
+}
+
+func (n NrExtension) InterceptResponse(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+	segment, ok := ctx.Value(operationSegmentContextKey{}).(*newrelic.Segment)
+	if ok && segment != nil {
+		defer segment.End()
+	}
 
 	return next(ctx)
 }
